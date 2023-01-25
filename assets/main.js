d2ffed79ee2a9594547a2f262e19fd4f8fe2c8b3
Caroline Trombeta <carolinecampost@gmail.com>
2023-01-25 13:49:25 +0000

    25.01 - Js refactored/created validations for input values type

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 function fillDataCard() {
     const inputName = document.getElementById('inputCardName');
-    const inputNumber = document.getElementById('inputCardNumber');
+    const inputNumber = (document.getElementById('inputCardNumber'));
     const inputExpDateM = document.getElementById('inputExpDateM');
     const inputExpDateY = document.getElementById('inputExpDateY');
     const inputCardCvc = document.getElementById('inputCardCvc');
@@ -10,65 +10,45 @@ function fillDataCard() {
     const divFillExpDateM = document.getElementById('fill-date-month');
     const divFillExpDateY = document.getElementById('fill-date-year');
     const divFillCardCvc = document.getElementById('fill-cvc');
+    const defaultValues = {
+        name: 'Jane Appleseed',
+        number: '0000 0000 0000 0000',
+        expDateM: '00',
+        expDateY: '00',
+        cvc: '000',
+    }
 
-    inputName.addEventListener('input', function(){
-        if (this.value) {
-            divFillName.innerHTML = this.value.toUpperCase();
-        } else if (this.value == '') {
-            divFillName.innerHTML = 'Jane Appleseed';
-        } else {
-            divFillName.innerHTML = 'Jane Appleseed';
-        }
+    const inputs = [inputName, inputNumber, inputExpDateM, inputExpDateY, inputCardCvc];
+    const divFills = [divFillName, divFillNumber, divFillExpDateM, divFillExpDateY, divFillCardCvc];
+
+
+    inputs.forEach((input, i) => {
+        input.addEventListener('input', function() {
+            if(input === inputName){
+                if(!/^[a-zA-Z ]+$/.test(this.value)) {
+                    alert('Please enter a valid name');
+                    this.value = '';
+                }
+            } else if(input === inputNumber || input === inputExpDateM || input === inputExpDateY || input === inputCardCvc){
+                if(isNaN(this.value)) {
+                    alert('Please enter a valid number');
+                    this.value = '';
+                }
+            }
+
+            divFills[i].innerHTML = this.value || defaultValues[Object.keys(defaultValues)[i]];
+        });
     });
 
-    inputNumber.addEventListener('input', function(){
-        if (this.value) {
-            divFillNumber.innerHTML = this.value;
-        } else if (this.value == '') {
-            divFillNumber.innerHTML = '0000 0000 0000 0000';
-        } else {
-            divFillNumber.innerHTML = '0000 0000 0000 0000';
-        }
-    });
 
-    inputExpDateM.addEventListener('input', function(){
-        if (this.value) {
-            divFillExpDateM.innerHTML = this.value;
-        } else if (this.value == '') {
-            divFillExpDateM.innerHTML = '00';
-        } else {
-            divFillExpDateM.innerHTML = '00';
-        }
-    });
-
-    inputExpDateY.addEventListener('input', function(){
-        if (this.value) {
-            divFillExpDateY.innerHTML = this.value;
-        } else if (this.value == '') {
-            divFillExpDateY.innerHTML = '00';
-        } else {
-            divFillExpDateY.innerHTML = '00';
-        }
-    });
-
-    inputCardCvc.addEventListener('input', function(){
-        if (this.value) {
-            divFillCardCvc.innerHTML = this.value;
-        } else if(this.value == '') {
-            divFillCardCvc.innerHTML = '000';
-        } else {
-            divFillCardCvc.innerHTML = '000';
-        }
-    });
-
-    // if (this.value) {
-    //     divFillName.innerHTML = this.value.toUpperCase();
-    // } else if (this.value == '') {
-    //     divFillName.innerHTML = 'Jane Appleseed';
-    // } else {
-    //     divFillName.innerHTML = 'Jane Appleseed';
-    // }
 }
-
 fillDataCard();
 
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
